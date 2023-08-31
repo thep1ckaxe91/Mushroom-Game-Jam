4e9f24d10ca4fdf0d2d7ac61a9a93283a473ddad
thep1ckaxe91 <98675539+thep1ckaxe91@users.noreply.github.com>
2023-08-31 19:43:27 +0700

    update

--- Scene.py
+++ Scene.py
@@ -3,51 +3,92 @@ from CONST import Game_CONST
 from UX_prop import *
 from typing import TYPE_CHECKING
 
+
 if TYPE_CHECKING:
     from main import Game
-'''
+"""
 draw func dont need clear and present, only need to pass the draw func
-'''
+"""
+
+
 class Scene:
-    def __init__(self,game : 'Game'):
+    def __init__(self, game: "Game"):
         self.game = game
-    def check_events(self): ...
-    def update(self): ...
-    def draw(self): ...
+
+    def check_events(self):
+        ...
+
+    def update(self):
+        ...
+
+    def draw(self):
+        ...
+
+
+def load_scene(game: "Game", scene: Scene, delay: int = 0):
+    """
+    Delay 'delay' ms and load a scene
+    """
+    pygame.time.wait(delay)
+    game.current_scene = scene
+
 
 class Level(Scene):
-    def __init__(self, game: 'Game'):
+    def __init__(self, game: "Game"):
         super().__init__(game)
 
-class MainMenu(Scene):
-    def __init__(self, game: 'Game'):
+
+class SaveFileChoose(Scene):
+    def __init__(self, game: "Game"):
         super().__init__(game)
-        
-        self.main_menu_bg = Texture.from_surface(game.renderer,pygame.image.load(Game_CONST.PATH + '/assets/graphics/general/main_menu_background.png'))
-        self.play_button = Button(
-            Game_CONST.PATH + '/assets/graphics/ux-ui/button/play',
-            (Game_CONST.SCR_WIDTH/2,Game_CONST.SCR_HEIGHT*2/3),
-            self.game,
-            print, #placeholder
-            ['play']
+
+
+class MainMenu(Scene):
+    def __init__(self, game: "Game"):
+        super().__init__(game)
+
+        mountain_bg = pygame.image.load(
+            Game_CONST.PATH + "/assets/graphics/general/mountain background.png"
         )
-        
+        mountain_bg.blit(
+            pygame.image.load(
+                Game_CONST.PATH + "/assets/graphics/general/mushroom cliff.png"
+            ),
+            (0, 0),
+        )
+        self.main_menu_bg = Texture.from_surface(
+            game.renderer,
+            mountain_bg
+        )
+        self.play_button = Button(
+            Game_CONST.PATH + "/assets/graphics/ux-ui/button/play",
+            (Game_CONST.SCR_WIDTH * 3 / 4, Game_CONST.SCR_HEIGHT * 2 / 3),
+            self.game,
+            print,  # placeholder
+            ["play"],
+        )
+
         self.options_button = Button(
-            Game_CONST.PATH + '/assets/graphics/ux-ui/button/options',
-            (Game_CONST.SCR_WIDTH/2,Game_CONST.SCR_HEIGHT*2/3 + Game_CONST.SCR_HEIGHT/9),
+            Game_CONST.PATH + "/assets/graphics/ux-ui/button/options",
+            (
+                Game_CONST.SCR_WIDTH * 3 / 4,
+                Game_CONST.SCR_HEIGHT * 2 / 3 + Game_CONST.SCR_HEIGHT / 9,
+            ),
             self.game,
             print,
-            ['options']
+            ["options"],
         )
 
         self.quit_button = Button(
-            Game_CONST.PATH + '/assets/graphics/ux-ui/button/quit',
-            (Game_CONST.SCR_WIDTH/2,Game_CONST.SCR_HEIGHT*2/3 + 2*Game_CONST.SCR_HEIGHT/9),
+            Game_CONST.PATH + "/assets/graphics/ux-ui/button/quit",
+            (
+                Game_CONST.SCR_WIDTH * 3 / 4,
+                Game_CONST.SCR_HEIGHT * 2 / 3 + 2 * Game_CONST.SCR_HEIGHT / 9,
+            ),
             self.game,
             exit,
-            [0]
-        )   
-
+            [0],
+        )
 
     def update(self):
         self.play_button.update()
@@ -55,7 +96,10 @@ class MainMenu(Scene):
         self.quit_button.update()
 
     def draw(self):
-        self.main_menu_bg.draw(self.main_menu_bg.get_rect(),pygame.Rect(0,0,self.game.window.size[0], self.game.window.size[1]))
+        self.main_menu_bg.draw(
+            self.main_menu_bg.get_rect(),
+            pygame.Rect(0, 0, self.game.window.size[0], self.game.window.size[1]),
+        )
         self.play_button.draw()
         self.options_button.draw()
         self.quit_button.draw()
--- main.py
+++ main.py
@@ -1,25 +1,28 @@
-import pygame,os
-from pygame._sdl2.video import Window,Texture,Image,Renderer
+import pygame, os
+from pygame._sdl2.video import Window, Texture, Image, Renderer
 from CONST import Game_CONST
 from Scene import *
 
 pygame.init()
 
-class Game:
 
+class Game:
     def __init__(self):
-        self.window = Window(Game_CONST.GAME_NAME,(Game_CONST.SCR_WIDTH,Game_CONST.SCR_HEIGHT))
+        self.window = Window(
+            Game_CONST.GAME_NAME, (Game_CONST.SCR_WIDTH, Game_CONST.SCR_HEIGHT)
+        )
+        self.window.position = [0,0]
         self.window.resizable = True
         self.renderer = Renderer(self.window)
-        self.current_scene : Scene = MainMenu(self)
+        self.current_scene: Scene = MainMenu(self)
 
         self.dt = -1
         self.clock = pygame.time.Clock()
 
     def update(self):
         self.clock.tick(Game_CONST.MAX_FPS)
-        self.dt = self.clock.get_time()*0.001
-        
+        self.dt = self.clock.get_time() * 0.001
+
         Game_CONST.update(self)
         self.current_scene.update()
 
@@ -37,12 +40,13 @@ class Game:
                 if event.type == pygame.QUIT:
                     running = False
                 elif event.type == pygame.VIDEORESIZE:
-                    Game_CONST.resize_update(self) 
+                    Game_CONST.resize_update(self)
             self.update()
             self.draw()
-            
+
         pygame.quit()
         self.window.destroy()
+
 
 if __name__ == "__main__":
     game = Game()
