bd5bbce2fa2da26e4bf9db2d1da113be76c1f187
thep1ckaxe91 <98675539+thep1ckaxe91@users.noreply.github.com>
2023-10-05 11:22:46 +0700

    update

--- test_run.py
+++ test_run.py
@@ -1,3 +1,12 @@
-import pygame
-a = pygame.Vector2()
-print(a == False)
+from math import *
+print("")
+s = input("enter equation: y = ")
+x = int(input("input x: "))
+s = s.replace("^","**")
+for i in range(10):
+   s = s.replace(f"{i}x",f"{i}*x")
+
+def f(x):
+    return eval(s)
+
+print(f(x))
